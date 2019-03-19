d80cf95810cf50c14170e370d5e20d393dc37a73
Wang-LC <wlc.9424@gmail.com>
2019-03-19 03:10:51 -0700

    add class team

--- NBA_data.py
+++ NBA_data.py
@@ -36,6 +36,33 @@ class Standing:
             print('{0:2} {1:40} {2:10}'.format(n, name, stats))
 
 
+class Team:
+    def __init__(self, name, record, ppg, opp, home_record, away_record, event):
+        self.name = name
+        self.record = record
+        self.ppg = ppg
+        self.opp = opp
+        self.home = home_record
+        self.away = away_record
+        self.event = event
+
+    def pprint(self):
+        # ----------------------------
+        # Team Name (record)
+        # Next Game:
+        # Points Per Game:
+        # Opponent Point Per Game:
+        # HOME:
+        # AWAY:
+        # ----------------------------
+        print('%s (%s)' % (self.name, self.record))
+        print('Next Game: %s' % self.event[0:10])
+        print('Points Per Game: %.1f' % self.ppg)
+        print('Opponent Point Per Game: %.1f' % self.opp)
+        print('HOME: %s' % self.home)
+        print('AWAY: %s' % self.away)
+
+
 # function to remove spaces, newlines, and other undesirable characters from strings
 def clean_data(raw_list, destination_list):
     for element in raw_list:
@@ -48,6 +75,10 @@ def clean_data(raw_list, destination_list):
 
 
 def today(website):  # today data program
+    """
+    :param website: si.com
+    :return: games' result/games' time (today)
+    """
     page = requests.get(website)
 
     tree = html.fromstring(page.text)
@@ -121,10 +152,21 @@ def load_url(url):
 
 
 def favourite_team(team):  # scraping favourite team next event by json
+    """
+    :param team: short name (name in teamshortname.txt)
+    :return: data of favourite team
+    """
     url_favor_team = 'http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams/%s' % team
     json_data = json.loads(load_url(url_favor_team))
-    print(team)
-    print('Next Game: %s' % json_data['team']['nextEvent'][0]['date'][0:9], '\n')
+    name = json_data['team']['displayName']
+    event = json_data['team']['nextEvent'][0]['date']
+    record = json_data['team']['record']['items'][0]['summary']
+    point_per_game = json_data['team']['record']['items'][0]['stats'][11]['value']
+    opponent_point = json_data['team']['record']['items'][0]['stats'][12]['value']
+    home_record = json_data['team']['record']['items'][1]['summary']
+    away_record = json_data['team']['record']['items'][2]['summary']
+
+    return Team(name, record, point_per_game, opponent_point, home_record, away_record, event)
 
 
 def standing():  # scraping standings by json
@@ -159,6 +201,6 @@ if __name__ == '__main__':
         url = 'https://www.si.com/nba/scoreboard'
     today(url)
     print('-'*50)
-    favourite_team('LAL')
+    favourite_team('LAL').pprint()
     print('-'*50)
     standing().pprint()
